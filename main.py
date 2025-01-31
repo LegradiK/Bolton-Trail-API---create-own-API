b63a832e1831da52b731b2ca0136da459066e0f1
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-31 15:14:37 +0000

    edited codes to make it possible to add/update data from postman

--- main.py
+++ main.py
@@ -56,6 +56,9 @@ def delete_trail(trail_id):
     trail_data = db.get_or_404(Trail, trail_id)
     db.session.delete(trail_data)
     db.session.commit()
+
+    db.session.remove()
+
     return jsonify(response={"success": f"Trail data {trail_id} has been successfully deleted."})
 
 @app.route('/update_data/<int:trail_id>', methods=["POST"])
@@ -96,6 +99,9 @@ def update_trail_data(trail_id):
         trail.has_cafe = has_cafe
 
     db.session.commit()
+
+    db.session.remove()
+
     return jsonify(response={"success": "Added a new trail data into the database."})
 
 
@@ -117,6 +123,9 @@ def add_new_trail():
     print(new_trail)
     db.session.add(new_trail)
     db.session.commit()
+
+    db.session.remove()
+
     return jsonify(response={"success": "Added a new trail data into the database."})
 
 
@@ -133,60 +142,17 @@ def search_trail():
         return jsonify({"error": "Please provide a location parameter"})
     searching_trails = db.session.execute(db.select(Trail).where(Trail.location == location)).scalars().all()
     if not searching_trails:
-        return jsonify({"error": f"No cafes found in {location}"})
+        return jsonify({"error": f"No trails found in {location}"})
     searched_trails = [trail.to_dict() for trail in searching_trails]
-    return jsonify(cafe=searched_trails)
+    return jsonify(trail=searched_trails)
 
 @app.route('/random', methods=['GET'])
 def get_random_trail():
     all_trails = db.session.execute(db.select(Trail)).scalars().all()
     random_trail = random.choice(all_trails)
-    return jsonify(cafe=random_trail.to_dict())
+    return jsonify(trail=random_trail.to_dict())
 
-    # return jsonify(cafe={
-    #         "id":random_cafe.id,
-    #         "name":random_cafe.name,
-    #         "map_url":random_cafe.map_url,
-    #         "img_url":random_cafe.img_url,
-    #         "location":random_cafe.location,
-    #         "has_sockets":random_cafe.has_sockets,
-    #         "has_sockets":random_cafe.has_sockets,
-    #         "has_toilet":random_cafe.has_toilet,
-    #         "has_wifi":random_cafe.has_wifi,
-    #         "can_take_calls":random_cafe.can_take_calls,
-    #         "seats":random_cafe.seats,
-    #         "coffee_price":random_cafe.coffee_price
-    #     })
-
-    # return jsonify(cafe={
-    #     #Omit the id from the response
-    #     # "id": random_cafe.id,
-    #     "name": random_cafe.name,
-    #     "map_url": random_cafe.map_url,
-    #     "img_url": random_cafe.img_url,
-    #     "location": random_cafe.location,
-        
-    #     #Put some properties in a sub-category
-    #     "amenities": {
-    #       "seats": random_cafe.seats,
-    #       "has_toilet": random_cafe.has_toilet,
-    #       "has_wifi": random_cafe.has_wifi,
-    #       "has_sockets": random_cafe.has_sockets,
-    #       "can_take_calls": random_cafe.can_take_calls,
-    #       "coffee_price": random_cafe.coffee_price,
-    #     }
-    # })
-
-
-
-# HTTP GET - Read Record
-
-# HTTP POST - Create Record
-
-# HTTP PUT/PATCH - Update Record
-
-# HTTP DELETE - Delete Record
-
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
